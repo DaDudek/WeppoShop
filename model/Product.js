392a345c601f2a5ec22e7f728a35157adb2eb6d9
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-29 18:58:53 +0100

    fix Product model

--- model/Product.js
+++ model/Product.js
@@ -1,24 +1,25 @@
-// const { DataTypes } = require('sequelize');
-//
-// module.exports = (sequelize) => {
-//     sequelize.define('product', {
-//         id: {
-//             allowNull: false,
-//             autoIncrement: true,
-//             primaryKey: true,
-//             type: DataTypes.INTEGER
-//         },
-//         name: {
-//             allowNull: false,
-//             type: DataTypes.STRING,
-//         },
-//         description: {
-//             allowNull: false,
-//             type: DataTypes.STRING
-//         },
-//         price: {
-//             allowNull: false,
-//             type: DataTypes.FLOAT
-//         },
-//     });
-// };
+import * as Sequelize from 'sequelize'
+import {sequelizeInstance} from "../database/db.js";
+
+const Product =
+    sequelizeInstance.define('product', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        name: {
+            allowNull: false,
+            type: Sequelize.STRING,
+        },
+        description: {
+            allowNull: false,
+            type: Sequelize.STRING
+        },
+        price: {
+            allowNull: false,
+            type: Sequelize.FLOAT
+        },
+    });
+export {Product};
