a47a2988526cb35d669ba58af2b8c74357bf15a2
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-31 13:41:21 +0100

    add basic paths

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import * as http from "http";
 import express from 'express'
 import {initRelations} from "./model/relations.js";
 import bodyParser from 'body-parser';
+import {UserController} from "./controller/UserController.js"
+import {ProductController} from "./controller/ProductController.js";
+import {OrderController} from "./controller/OrderController.js";
 
 let app = express();
 
@@ -14,6 +17,7 @@ app.set('views', './views');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
+
 //
 // app.use( (req, res) => {
 //     res.render('index', {username: 'foo'});
@@ -29,6 +33,88 @@ app.post('/order', (req, res) => {
 app.get('/init', (req, res) => {
     test_table(req, res);
 });
+
+// USER
+app.get('/user', (req, res) => {
+    const userController = new UserController();
+    userController.getAll(req, res);
+})
+
+app.post('/user', (req, res) => {
+    const userController = new UserController();
+    userController.create(req, res);
+})
+
+app.delete('/user/:id', (req, res) => {
+    const userController = new UserController();
+    userController.remove(req, res);
+})
+
+app.get('/user/:id', (req, res) => {
+    const userController = new UserController();
+    userController.getById(req, res);
+})
+
+//PRODUCT
+app.get('/product', (req, res) => {
+    const productController = new ProductController();
+    if (req.query.name){
+        return productController.getByName(req, res);
+    }
+    if (req.query.description){
+        return productController.getByDescription(req, res)
+    }
+    return productController.getAll(req, res);
+})
+
+app.post('/product', (req, res) => {
+    const productController = new ProductController();
+    productController.create(req, res);
+})
+
+app.delete('/product/:id', (req, res) => {
+    const productController = new ProductController();
+    productController.remove(req, res);
+})
+
+app.get('/product/:id', (req, res) => {
+    const productController = new ProductController();
+    productController.getById(req, res);
+})
+
+
+//ORDER
+app.delete('/user/:id', (req, res) => {
+    const userController = new UserController();
+    userController.remove(req, res);
+})
+
+app.get('/user/:id', (req, res) => {
+    const userController = new UserController();
+    userController.getById(req, res);
+})
+
+//PRODUCT
+app.get('/order', (req, res) => {
+    const orderController = new OrderController();
+    orderController.getAll(req, res);
+})
+
+app.post('/order', (req, res) => {
+    const orderController = new OrderController();
+    orderController.create(req, res);
+})
+
+app.delete('/order/:id', (req, res) => {
+    const orderController = new OrderController();
+    orderController.remove(req, res);
+})
+
+app.get('/order/:id', (req, res) => {
+    const orderController = new OrderController();
+    orderController.getById(req, res);
+})
+
 
 const start = async () => {
     try {
