526e2c2515d1c731dc8de7d70b2e53617e12f6a2
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-31 13:40:53 +0100

    add product controller

---
+++ repository/ProductRepository.js
@@ -0,0 +1,54 @@
+import {Product} from "../model/Product.js";
+import * as Sequelize from 'sequelize'
+
+class ProductRepository{
+    async create(product){
+        return Product.create(product);
+    }
+
+    async getAll(){
+        return await Product.findAll();
+    }
+
+    async getById(productId){
+        return await Product.findAll({
+            where: {
+                id: productId
+            }
+        })
+    }
+
+    async update(product){
+        return await product.save();
+    }
+
+    async remove(productId){
+        return await Product.destroy({
+            where: {
+                id: productId
+            }
+        });
+    }
+
+    async getByName(productName){
+        return await Product.findAll({
+            where: {
+                name: {
+                    [Sequelize.Op.like]: `%${productName}%`
+                }
+            }
+        })
+    }
+
+    async getByDescription(productDescription){
+        return await Product.findAll({
+            where: {
+                description: {
+                    [Sequelize.Op.like]: `%${productDescription}%`
+                }
+            }
+        })
+    }
+}
+
+export {ProductRepository}
