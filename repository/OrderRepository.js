7af4090e75cb139b4329e0e951a9b5d162891336
Dawid Dudek <dawid.dudek@fingo.pl>
2022-01-31 13:42:16 +0100

    cleaning code

--- repository/OrderRepository.js
+++ repository/OrderRepository.js
@@ -2,24 +2,14 @@ import {Order} from "../model/Order.js";
 
 class OrderRepository{
     async create(order){
-        // const createdOrder = await Order.create(order);
         return Order.create(order);
     }
 
     async getAll(){
-        // const orders = await Order.findAll();
-        // return orders;
         return await Order.findAll();
     }
 
     async getById(orderId){
-        // const order = await Order.findAll({
-        //             where: {
-        //                 id: req.params.id
-        //             }
-        //         })
-        // return order
-
         return await Order.findAll({
                         where: {
                             id: orderId
@@ -32,13 +22,11 @@ class OrderRepository{
     }
 
     async remove(orderId){
-        // const numberOfRemoved
         return await Order.destroy({
                 where: {
                     id: orderId
                 }
             });
-        // return numberOfRemoved;
     }
 }
 
